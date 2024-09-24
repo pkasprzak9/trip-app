4353afe88e5741df986c6f5ba440ce56a67ba930
pkasprzak9 <pawkasprzak9@gmail.com>
2024-09-24 14:55:35 +0200

    Add text-decoration-underline to login link

--- src/components/StartPage/NavBar.jsx
+++ src/components/StartPage/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Container, Offcanvas, Button } from "react-bootstrap";
+import { Display } from "react-bootstrap-icons";
 
 export default function NavBar() {
   return (
@@ -16,13 +17,13 @@ export default function NavBar() {
             <Offcanvas.Title id="offcanvasNavbarLabel" className=" fw-bold fs-4 text-primary">Pathify</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body className="d-flex flex-column flex-lg-row">
-            <Nav className="justify-content-center align-items-center gap-3 flex-grow-1 pe-3">
+            <Nav className="justify-content-center align-items-center gap-3 flex-grow-1">
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#">Features</Nav.Link>
               <Nav.Link href="#">How It Works</Nav.Link>
             </Nav>
             <Nav className="d-flex flex-row justify-content-center align-items-center mb-5 mb-lg-0">
-              <Nav.Link href="#" className=" me-3">Login</Nav.Link>
+              <Nav.Link href="#" className=" me-3 text-decoration-underline">Login</Nav.Link>
               <Button variant="primary">Sign Up</Button>
             </Nav>
           </Offcanvas.Body>
