1178c44e46a3ba4903fbc414a034559249865062
pkasprzak9 <pawkasprzak9@gmail.com>
2024-09-26 13:05:47 +0200

    Implement Benefits section

---
+++ src/components/StartPage/Benefits.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Gear, LightningCharge, Phone, Clock  } from "react-bootstrap-icons";
+
+export default function Benefits() {
+  return (
+    <section id="features" className="bg-light">
+      <Container fluid="md" className="py-5">
+        <div className="text-center px-5">
+          <h2 className="text-primary">Why Planning with Pathify Feels Different</h2>
+          <p className="lead text-muted">
+            Discover the key benefits that make your travel planning seamless and enjoyable.
+          </p>
+        </div>
+
+        <Row className="align-items-stretch g-5 g-lg-3 justify-content-center mt-5 py-5">
+        <Col xs="10" lg="5" className="text-center">
+            <LightningCharge width={50} height={50} className="text-primary mb-3"></LightningCharge>
+            <h3 className="text-primary">Speed</h3>
+            <p>Plan your perfect route in seconds with the power of AI.</p>
+          </Col>
+
+          <Col xs="10" lg="5" className="text-center">
+            <Gear width={50} height={50} className="text-primary mb-3"></Gear>
+            <h3 className="text-primary">Customization</h3>
+            <p>Tailor your routes to fit your preferences any time you want.</p>
+          </Col>
+
+          <Col xs="10" lg="5" className="text-center">
+            <Phone width={50} height={50} className="text-primary mb-3"></Phone>
+            <h3 className="text-primary">Always Accessible</h3>
+            <p>Your routes and preferences are always available on your device, whenever you need them.</p>
+          </Col>
+
+          <Col xs="10" lg="5" className="text-center">
+            <Clock width={50} height={50} className="text-primary mb-3"></Clock>
+            <h3 className="text-primary">Convenience</h3>
+            <p>Plan and adjust your travels from anywhere, at any time.</p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
