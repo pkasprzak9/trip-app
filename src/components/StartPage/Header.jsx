653368a402e1c77877d0c2632709373c54f8deda
pkasprzak9 <pawkasprzak9@gmail.com>
2024-09-27 13:30:15 +0200

    Refactor Header component to improve accessibility and responsiveness

--- src/components/StartPage/Header.jsx
+++ src/components/StartPage/Header.jsx
@@ -11,17 +11,13 @@ export default function Header() {
       <Container fluid="md py-5">
         <Row className="justify-content-center align-items-center py-5">
           <Col className="text-center text-lg-start" xs={{span: 10, order: 1}} lg={{span: 6, order: 0}}>
-            <h1>
-              <div className="display-3 mb-3 text-primary">Discover Your Next Adventure</div>
-              <div className="display-5 text-muted">
-              Personalized travel routes powered by AI to make your journey unforgettable.
-              </div>
-            </h1>
+            <h1 className="display-3 mb-3 text-primary">Discover Your Next Adventure</h1>
+            <p className="display-5 text-muted">Personalized travel routes powered by AI to make your journey unforgettable.</p>
             <Button as="a" href="#about" size="lg" variant="outline-primary" className="mt-5 me-3">Learn More</Button>
             <Button size="lg" className="mt-5">Start Now</Button>
           </Col>
           <Col xs="8" lg="6" className="d-none d-lg-block mb-5 mb-lg-0">
-            <img src={headerImage} className="img-fluid" alt="" />
+            <img src={headerImage} className="img-fluid" alt="Illustrated man standing in front of the earth globe with map in his hand" />
           </Col>
         </Row>
       </Container>
