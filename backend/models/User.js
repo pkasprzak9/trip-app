73c424b49eff059cb85b0f697defd99555e49f15
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-10 12:24:46 +0200

    Add User model for backend authentication

---
+++ backend/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
