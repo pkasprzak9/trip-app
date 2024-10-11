f18f74fea1ebe1df694ba68ed9358ad9849e0136
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-11 19:25:39 +0200

    Add PrivateRoute component for authenticated routes

---
+++ src/components/UserPanel/Dashboard.jsx
@@ -0,0 +1,10 @@
+// src/components/Dashboard.jsx
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="text-center mt-5">
+      <h1>You're logged in!</h1>
+    </div>
+  );
+}
