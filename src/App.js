205d19c37970da5925f5f049e0cba6d17cfdf300
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-11 17:52:24 +0200

    Refactor routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import StartPage from "./components/StartPage/StartPage";
 import Login from "./components/Auth/Login"
 import Register from "./components/Auth/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <StartPage></StartPage>
-      {/* <Login></Login> */}
-      {/* <Register></Register> */}
-    </>
+    <Router>
+      <Routes>
+        {/* Strona Główna */}
+        <Route path="/" element={<StartPage/>} />
+        {/* Strona Rejestracji */}
+        <Route path="/register" element={<Register />} />
+        {/* Strona Logowania */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    // <>
+    //   {/* <StartPage></StartPage> */}
+    //   {/* <Login></Login> */}
+    //   <Register></Register>
+    // </>
   )
 }
 
