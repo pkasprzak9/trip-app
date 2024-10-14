5434db8c6b5f087b7bacb14f106159950492782f
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-14 18:12:30 +0200

    Implement Dashboard component and add there NavBar

--- src/components/UserPanel/Dashboard.jsx
+++ src/components/UserPanel/Dashboard.jsx
@@ -1,10 +1,16 @@
 // src/components/Dashboard.jsx
 import React from "react";
+import NavBar from "./NavBar";
 
 export default function Dashboard() {
   return (
+    <>
+    <NavBar>
+
+    </NavBar>
     <div className="text-center mt-5">
       <h1>You're logged in!</h1>
     </div>
+    </>
   );
 }
