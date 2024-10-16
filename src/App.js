8fb4b09f4c8b2944eddab4e1499c2eadfe905bf3
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-16 20:01:27 +0200

    Refactor App.js to implement context providers for user authentication and data management

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Register from "./components/Auth/Register";
 import Dashboard from "./components/UserPanel/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StartPage/>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        {/* Chroniona trasa */}
-        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<StartPage/>} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            {/* Chroniona trasa */}
+            <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+          </Routes>
+        </Router>
+      </UserProvider>
+    </AuthProvider>
   );
 }
 
