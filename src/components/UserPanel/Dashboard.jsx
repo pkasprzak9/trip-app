672c27c3f9edd61c85910909c5249cc7c1d17739
pkasprzak9 <pawkasprzak9@gmail.com>
2024-10-16 20:01:58 +0200

    Refactor Dashboard component to display user data in NavBar

--- src/components/UserPanel/Dashboard.jsx
+++ src/components/UserPanel/Dashboard.jsx
@@ -1,14 +1,18 @@
 // src/components/Dashboard.jsx
-import React from "react";
+import React, { useContext } from "react";
 import NavBar from "./NavBar";
+import { UserContext } from "../../context/UserContext";
 
 export default function Dashboard() {
+  const { userData } = useContext(UserContext);
+
+  console.log(userData);
   return (
     <>
     <NavBar>
-
     </NavBar>
     <div className="text-center mt-5">
+      <h1>Hello {userData.firstName} {userData.lastName}</h1>
       <h1>You're logged in!</h1>
     </div>
     </>
