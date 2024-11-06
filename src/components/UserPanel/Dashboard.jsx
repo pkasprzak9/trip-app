3fc5ccaf9c6bf78d41bee7473d78833382184d0d
pkasprzak9 <pawkasprzak9@gmail.com>
2024-11-06 11:07:32 +0100

    Refactor Dashboard component to remove unnecessary code and update UI rendering

--- src/components/UserPanel/Dashboard.jsx
+++ src/components/UserPanel/Dashboard.jsx
@@ -1,20 +1,9 @@
-// src/components/Dashboard.jsx
-import React, { useContext } from "react";
-import NavBar from "./NavBar";
-import { UserContext } from "../../context/UserContext";
+import React from "react";
 
 export default function Dashboard() {
-  const { userData } = useContext(UserContext);
-
-  console.log(userData);
   return (
     <>
-    <NavBar>
-    </NavBar>
-    <div className="text-center mt-5">
-      <h1>Hello {userData.firstName} {userData.lastName}</h1>
-      <h1>You're logged in!</h1>
-    </div>
+      Dashboard
     </>
-  );
+  )
 }
