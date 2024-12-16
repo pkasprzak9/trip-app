009783c08d24ef042b039b6fb18207e0cf3295ef
pkasprzak9 <pawkasprzak9@gmail.com>
2024-12-17 00:23:56 +0100

    Refactor routing in App component to include nested private routes for UserPanel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StartPage from "./components/StartPage/StartPage";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import UserPanel from "./components/UserPanel/UserPanel";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
 import { UserProvider } from "./context/UserContext";
+import UserPanel from "./components/UserPanel/UserPanel";
+import Dashboard from "./components/UserPanel/Dashboard";
+import NewRouteForm from "./components/UserPanel/NewRouteForm";
+import DisplayRoute from "./components/UserPanel/DisplayRoute";
 
 function App() {
   return (
@@ -13,11 +18,25 @@ function App() {
       <UserProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<StartPage/>} />
+            {/* Public routes */}
+            <Route path="/" element={<StartPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            {/* Chroniona trasa */}
-            <Route path="/dashboard" element={<PrivateRoute element={<UserPanel />} />} />
+
+            {/* Private routes with layout */}
+            <Route
+              path="/dashboard/*"
+              element={
+                <PrivateRoute>
+                  <UserPanel />
+                </PrivateRoute>
+              }
+            >
+              {/* Nested routes inside UserPanel */}
+              <Route path="" element={<Dashboard />} />
+              <Route path="new" element={<NewRouteForm />} />
+              <Route path="display" element={<DisplayRoute />} />
+            </Route>
           </Routes>
         </Router>
       </UserProvider>
