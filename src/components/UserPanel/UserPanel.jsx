62b221b1cbd80211cbf23320a12bfd46d874ead8
pkasprzak9 <pawkasprzak9@gmail.com>
2024-12-17 00:25:23 +0100

    Refactor UserPanel component to simplify structure and integrate routing

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,24 +1,15 @@
-// src/components/Dashboard.jsx
-import React, { useContext } from "react";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
-import Dashboard from "./Dashboard";
-import NewRouteForm from "./NewRouteForm";
-import { UserContext } from "../../context/UserContext";
+import { Container } from "react-bootstrap";
 
 export default function UserPanel() {
-  const { userData } = useContext(UserContext);
-
-  console.log(userData);
   return (
     <>
-    <NavBar>
-    </NavBar>
-    {/* <div className="text-center mt-5">
-      <h1>Hello {userData.firstName} {userData.lastName}</h1>
-      <h1>You're logged in!</h1>
-    </div> */}
-    <NewRouteForm></NewRouteForm>
-    {/* <Dashboard></Dashboard> */}
+      <NavBar />
+      <Container className="mt-5">
+        <Outlet />
+      </Container>
     </>
   );
 }
