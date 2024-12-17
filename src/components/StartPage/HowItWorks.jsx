a319ac184c0ea9cf71bf9d0fcf62adccbf74325a
pkasprzak9 <pawkasprzak9@gmail.com>
2024-12-17 21:59:49 +0100

    Refactor HowItWorks component to improve comment clarity and enhance readability

--- src/components/StartPage/HowItWorks.jsx
+++ src/components/StartPage/HowItWorks.jsx
@@ -6,18 +6,24 @@ import thirdImage from "../../assets/images/Delivery-location.svg";
 import fourthImage from "../../assets/images/Outer-space.svg";
 
 export default function HowItWorks() {
+  // można w przyszłości ulepszyć o dynamiczne renderowanie treści (trzymanie w obiekcie)
   return (
     <section id="howItWorks">
       <Container fluid="md" className="py-5">
+        {/* NAGŁÓWEK SEKCJI */}
         <div className="text-center px-5">
           <h2 className="text-primary">The Future of Travel Planning</h2>
           <p className="lead text-muted">Discover how AI revolutionizes travel, putting your preferences at the forefront.</p>
         </div>
 
         <Row className="justify-content-center align-items-center py-5 mt-5">
+          {/* KARUZELA */}
+          {/* domyślnie zajmuje 12 kolumn, dopiero od lg zajmuje 6 */}
           <Col lg="6">
             <Carousel aria-live="polite">
+              {/* interval: czas przejścia karuzeli */}
               <Carousel.Item interval={5000} className="text-center px-5">
+                {/* img-fluid dla responsywności */}
                 <img src={firstImage} className="img-fluid px-5"  alt="Man at the airport looking at his phone, preparing for travel"/>
                 <h3 className="heading display-5">Set Your Preferences</h3>
                 <p className="lead mb-5 text-muted">
@@ -35,6 +41,7 @@ export default function HowItWorks() {
                 <img src={thirdImage} alt="traveler checking map on smartphone while sitting on a scooter" className="img-fluid px-5" />
                 <h3 className="heading display-5">View Your Route</h3>
                 <p className="lead mb-5 text-muted">
+                  {/* Nie ma na razie tej funkcjonalności */}
                   Visualize your customized route with all your desired stops perfectly mapped.
                 </p>
               </Carousel.Item>
@@ -42,6 +49,7 @@ export default function HowItWorks() {
                 <img src={fourthImage} alt="Man inside a car, orbiting a planet" className="img-fluid px-5" />
                 <h3 className="heading display-5">Save & Customize</h3>
                 <p className="lead mb-5 text-muted">
+                  {/* Nie ma na razie tej funkcjonalności */}
                   Save your preferences for future trips, or make adjustments to fit your current plans.
                 </p>
               </Carousel.Item>
