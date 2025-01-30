e28f6717d1db6d96b0d7a443fd96fc8d397bb441
pkasprzak9 <pawkasprzak9@gmail.com>
2025-01-30 12:33:26 +0100

    Refactor server.js to improve code clarity with enhanced comments and organization

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const cors = require('cors'); // Pozwala na akceptację żądań z innych domen tj. frontu działającego na innym serwerze
 const registerRoutes = require('./routes/register');
 const authRoutes = require('./routes/auth');
 const generateRoutes = require('./routes/generate');
-require('dotenv').config();
+require('dotenv').config(); // Wczytuje zmienne środowiskowe
 
+// TWORZENIE INSTANCJI SERWERA EXPRESS
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // 5001 || 5000
 
 // MIDDLEWARE
+// cors() -> pozwala na komunikację frontu z backendem
+// express.json() -> pozwala na przetwarzanie przychodzących danych w formacie JSON
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
+// DEFINIOWANIE TRAS API
 app.use('/api/register', registerRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/generate-route', generateRoutes)
 
-// MongoDB CONNECTION
+// POŁĄCZENIE Z MONGO DB
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI); // GLOBALNY KONTEKST DLA CAŁEJ APLIKACJI
     console.log('Connected to MongoDB.');
     app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
   } catch (err) {
